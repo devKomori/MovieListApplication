d9428571db47423543192f475bdaea99a8c8b92e
Brennan <172959925+devKomori@users.noreply.github.com.>
2024-07-27 02:20:35 -0700

    Completed Login page layout. Updated gradle to have viewBinding. Need to implement the LoginActivity.

---
+++ app/src/main/java/com/example/movielistapplication/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.movielistapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.movielistapplication.databinding.ActivityLoginBinding;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+
+
+}
