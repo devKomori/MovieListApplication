41d9c34bb0e4c60461a98cdff0c4b3eef00e2476
Brennan <172959925+devKomori@users.noreply.github.com.>
2024-07-31 16:12:08 -0700

    Updated the MainActivity onCreate to inflate the proper layout for a user's role. Added the OnClickListener to start the intent for BrowseMoviesActivity.

--- app/src/main/java/com/example/movielistapplication/BrowseMoviesActivity.java
+++ app/src/main/java/com/example/movielistapplication/BrowseMoviesActivity.java
@@ -1,16 +1,36 @@
 package com.example.movielistapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.movielistapplication.Database.MovieListRepository;
+import com.example.movielistapplication.databinding.ActivityBrowseMoviesBinding;
+
 
 public class BrowseMoviesActivity extends AppCompatActivity {
 
+    private ActivityBrowseMoviesBinding binding;
+    private MovieListRepository repository;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityBrowseMoviesBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_browse_movies);
+        setContentView(binding.getRoot());
 
     }
+
+
+
+
+    public static Intent browseMoviesIntentFactory(Context context) {
+        return new Intent(context, BrowseMoviesActivity.class);
+    }
+
+
+
 }
