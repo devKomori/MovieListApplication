acd73f4696cc6f8cdb33f06288ddfc55a98a1e30
Brennan <172959925+devKomori@users.noreply.github.com.>
2024-07-31 14:20:44 -0700

    Created BrowseMoviesActivity

---
+++ app/src/main/java/com/example/movielistapplication/BrowseMoviesActivity.java
@@ -0,0 +1,16 @@
+package com.example.movielistapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class BrowseMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_movies);
+
+    }
+}
