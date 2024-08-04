e87242a5a99d5fbdd9a50f6c1b6a64f4935d861f
Brennan <172959925+devKomori@users.noreply.github.com.>
2024-08-03 23:55:58 -0700

    Added search request in the TMDBRequest. Going to try and implement a search bar next.

--- app/src/main/java/com/example/movielistapplication/Database/TMDBRequest.java
+++ app/src/main/java/com/example/movielistapplication/Database/TMDBRequest.java
@@ -7,9 +7,17 @@ import retrofit2.http.Query;
 public interface TMDBRequest {
   String API_KEY = "eaab2dae638911e60d7907ca349f1855";
 
-  @GET("3/movie/popular")
+  @GET("/3/movie/popular")
   Call<MovieApiJsonResponse> getPopularMovies(
           @Query("api_key") String key,
+          @Query("page") String page
+  );
+
+
+  @GET("/3/search/movie")
+  Call<MovieApiJsonResponse> searchMovies(
+          @Query("api_key") String key,
+          @Query("query") String query,
           @Query("page") String page
   );
 
